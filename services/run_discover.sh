4a71cedc2e506e15096f7b16e64bb8bd819b14f1
brett <brettgraham@gmail.com>
2020-06-04 13:41:30 -0400

    fix #16: modified discover to run as pi and set owner of /dev/shm/pcam

--- services/run_discover.sh
+++ services/run_discover.sh
@@ -9,4 +9,9 @@ MY_IP=`ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1`
 CIDR=$MY_IP/24
 
 echo "Running discover on network $CIDR"
+if [ ! -d "/dev/shm/pcam" ]; then
+  mkdir /dev/shm/pcam
+  chown pi /dev/shm/pcam
+  chgrp pi /dev/shm/pcam
+fi
 python3 -m pollinatorcam discover -v -i $MY_IP/24
