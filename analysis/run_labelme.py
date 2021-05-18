a8bc3cbc25f23bac0020a3c6a9b6869a6bae44e1
Brett <brettgraham@gmail.com>
2021-05-18 16:24:32 -0400

    add bbox support to run_labelme

--- analysis/run_labelme.py
+++ analysis/run_labelme.py
@@ -68,6 +68,9 @@ parser.add_argument(
 parser.add_argument(
     '-r', '--resume', default=False, action='store_true',
     help="Resume from a previous (crashed) annotation")
+parser.add_argument(
+    '-s', '--start_end_limit', default=False, action='sore_true',
+    help="Limit images to those between start and end tags")
 
 args = parser.parse_args()
 if args.verbose:
@@ -79,29 +82,6 @@ min_time = day + datetime.timedelta(hours=args.first_hour)
 max_time = day + datetime.timedelta(hours=args.last_hour)
 
 db = sqlite3.connect(args.database_filename, detect_types=sqlite3.PARSE_DECLTYPES)
-
-
-tags = {0: 'note', 1: 'start', 2: 'end'}
-labels = {0: 'note', 1: 'flower', 2: 'pollinator'}
-annotation_template = {
-    "version": "4.5.6",
-    "shapes": [],
-    "imagePath": "",  # fill with tfn
-    "imageData": None,
-    "imageHeight": 1944,
-    "imageWidth": 2592,
-}
-shape_template = {
-    "label": "",  # fill with label
-    "points": [],  # append [x, y] as list
-    "groupd_id": None,
-    "shape_type": "point",
-    "flags": {},
-}
-flags_template = {name: False for name in iter(tags.values())}
-
-rtags = {v: k for (k, v) in tags.items()}
-rlabels = {v: k for (k, v) in labels.items()}
 
 
 def table_exists(db, table_name):
@@ -121,10 +101,10 @@ if not table_exists(db, 'tag_names'):
         "tag_id INTEGER PRIMARY KEY,"
         "name TEXT"
         ");")
-    for tag_id in tags:
+    for tag in ('note', 'start', 'end'):
         db.execute(
-            "INSERT INTO tag_names (tag_id, name) VALUES (?, ?);",
-            (tag_id, tags[tag_id]))
+            "INSERT INTO tag_names (name) VALUES (?);",
+            (tag, ))
     db.commit()
 # - label_names: (label id[int], label name[str])
 if not table_exists(db, 'label_names'):
@@ -134,10 +114,10 @@ if not table_exists(db, 'label_names'):
         "label_id INTEGER PRIMARY KEY,"
         "name TEXT"
         ");")
-    for label_id in labels:
+    for label in ('note', 'flower', 'pollinator'):
         db.execute(
-            "INSERT INTO label_names (label_id, name) VALUES (?, ?);",
-            (label_id, labels[label_id]))
+            "INSERT INTO label_names (name) VALUES (?);",
+            (label, ))
     db.commit()
 # - tags: (still id[int], tag id[int])  # can be multiple per image
 if not table_exists(db, 'tags'):
@@ -161,15 +141,103 @@ if not table_exists(db, 'labels'):
         "y INTEGER"
         ");")
     db.commit()
+# - bbox_labels: (bbox_label_id[int], name[str])
+if not table_exists(db, 'bbox_labels'):
+    db.execute(
+        "CREATE TABLE bbox_labels ("
+        "bbox_label_id INTEGER PRIMARY KEY,"
+        "name TEXT"
+        ");")
+    db.commit()
+# - bboxes: (
+#    bbox_id[int], still_id[int], label_id[int],
+#    left[real], top[real], right[real], bottom[real])
+if not table_exists(db, 'bboxes'):
+    db.execute(
+        "CREATE TABLE bboxes ("
+        "bbox_id INTEGER PRIMARY KEY,"
+        "still_id INTEGER,"
+        "label_id INTEGER,"
+        "left REAL,"
+        "top REAL,"
+        "right REAL,"
+        "bottom, REAL"
+        ");")
+    db.commit()
+
+
+def load_lookups_from_table(table_name):
+    table = dict(db.execute("SELECT * FROM " + table_name).fetchall())
+    rtable = {v: k for (k, v) in table.items()}
+    assert len(table) == len(rtable)
+
+tags, rtags = load_lookups_from_table('tag_names')
+labels, rlabels = load_lookups_from_table('label_names')
+bbox_labels, rbbox_labels = load_lookups_from_table('bbox_labels')
+# tag_names_by_code = db.execute(
+#     "SELECT tag_id, name FROM tag_names").fetchall()
+# tags = dict(tag_names_by_code)
+# 
+# label_names_by_code = db.execute(
+#     "SELECT label_id, name FROM label_names").fetchall()
+# if len(label_names_by_code) == 0:
+#     labels = {0: 'note', 1: 'flower', 2: 'pollinator'}
+# else:
+#     labels = dict(label_names_by_code)
+# 
+# bbox_labels_by_code = db.execute(
+#     "SELECT bbox_label_id, name FROM bbox_labels").fetchall()
+# if len(label_names_by_code) == 0:
+#     bbox_labels = {}
+# else:
+#     bbox_labels = dict(bbox_labels_by_code)
+ 
+annotation_template = {
+    "version": "4.5.6",
+    "shapes": [],
+    "imagePath": "",  # fill with tfn
+    "imageData": None,
+    "imageHeight": 1944,
+    "imageWidth": 2592,
+}
+shape_template = {
+    "label": "",  # fill with label
+    "points": [],  # append [x, y] as list
+    "groupd_id": None,
+    "shape_type": "point",
+    "flags": {},
+}
+flags_template = {name: False for name in iter(tags.values())}
+
+#rtags = {v: k for (k, v) in tags.items()}
+#rlabels = {v: k for (k, v) in labels.items()}
+#rbbox_labels = {v: k for (k, v) in labels.items()}
 
 # get fns from database selecting for camera and time
 file_infos = []
+
+# subset by start/end tagged images
+if args.start_end_limit:
+    found_start = False
+    found_end = False
 for s in db.execute(
         "SELECT * FROM stills WHERE "
         "camera_id=? AND "
         "timestamp>=? AND timestamp<=?;",
         (args.camera_id, min_time, max_time)):
     still_id, args.camera_id, timestamp, path = s
+    if args.start_end_limit:
+        if found_end:
+            # end was added on the previous iteration
+            break
+        image_tags = [r[0] for r in db.execute(
+            'SELECT tag_id FROM tags WHERE still_id=?', (still_id, )).fetchall()]
+        if not found_start:
+            if rtags['start'] in image_tags:
+                found_start = True
+        else:
+            if rtags['end'] in image_tags:
+                found_end = True
     file_infos.append({
         'path': os.path.join(args.data_dir, path),
         'timestamp': timestamp,
@@ -235,7 +303,8 @@ for (index, fi) in enumerate(file_infos):
         previous_tags.append(tags[r[0]])
 
     previous_labels = []
-    for r in db.execute("SELECT label_id, x, y FROM labels WHERE still_id=?", (still_id, )):
+    for r in db.execute(
+            "SELECT label_id, x, y FROM labels WHERE still_id=?", (still_id, )):
         logging.debug(f"Found previous point {r} for {still_id}")
         previously_annotated_images.add(still_id)
         previous_labels.append({
@@ -243,8 +312,19 @@ for (index, fi) in enumerate(file_infos):
             'xy': (r[1], r[2]),
         })
 
+    previous_bboxes = []
+    for r in db.execute(
+            "SELECT bbox_id, left, top, right, bottom FROM bboxes WHERE still_id=?",
+            (still_id, )):
+        logging.debug(f"Found previous bboxes {r} for {still_id}")
+        previously_annotated_images.add(still_id)
+        previous_bboxes.append({
+            'name': bbox_labels[r[0]],
+            'points': [[r[1], r[2]], [r[3], r[4]]],
+        })
+
     # write out json for any previous annotations
-    if len(previous_tags) or len(previous_labels):
+    if len(previous_tags) or len(previous_labels) or len(previous_bboxes):
         annotation = copy.deepcopy(annotation_template)
         annotation['flags'] = copy.deepcopy(flags_template)
         for tag in previous_tags:
@@ -254,6 +334,11 @@ for (index, fi) in enumerate(file_infos):
             shape["label"] = label["name"]
             shape["points"].append(label["xy"])
             annotation['shapes'].append(shape)
+        for bbox in previous_bboxes:
+            shape = copy.deepcopy(shape_template)
+            shape['shape_type'] = 'rectangle'
+            shape['label'] = bbox['name']
+            shape['points'] = bbox['points']
         annotation["imagePath"] = tfn
         jfn = os.path.join(args.tmp_dir, os.path.splitext(tfn)[0] + ".json")
         with open(jfn, "w") as f:
@@ -264,15 +349,22 @@ if args.resume and len(previous_image_fns) != 0:
     raise Exception("Failing to resume because not all temp files were found in db")
 
 # run labelme to annotate images
+# some tag and label names have spaces, will these work in command or
+# will they need to be written to a separate file?
+tag_names = "'" + ",".join(sorted(list(tags.values))) + "'"
+label_names = (
+    "'" +
+    ",".join(sorted(set(labels.values()).union(set(bbox_labels.values())))) +
+    ",")
 cmd = [
     "labelme",
     args.tmp_dir,
     "--config",
     "labelmerc",
     "--flags",
-    ",".join(sorted(list(tags.values()))),
+    tag_names,
     "--labels",
-    ",".join(sorted(list(labels.values()))),
+    label_names,
 ]
 subprocess.check_call(cmd)
 
@@ -281,6 +373,7 @@ for still_id in previously_annotated_images:
     logging.debug(f"Removing previous annotations for {still_id}")
     db.execute("DELETE FROM tags WHERE still_id=?", (still_id, ))
     db.execute("DELETE FROM labels WHERE still_id=?", (still_id, ))
+    db.execute("DELETE FROM bboxes WHERE still_id=?", (still_id, ))
 
 # parse annotations
 annotation_filenames = sorted(glob.glob(os.path.join(args.tmp_dir, '*.json')))
@@ -311,22 +404,58 @@ for afn in annotation_filenames:
 
         # save labels
         for s in data['shapes']:
-            if s['shape_type'] != 'point':
+            if s['shape_type'] == 'point':
+                pts = s['points']
+                assert len(pts) == 1
+                x, y = pts[0]
+                # check for new label
+                if s['label'] not in rlabels:
+                    # insert into database
+                    db.execute(
+                        'INSERT INTO label_names (name) VALUES (?);',
+                        (s['label'], ))
+                    db.commit()
+                    # reload labels
+                    labels, rlabels = load_lookups_from_table('label_names')
+                label_id = rlabels[s['label']]
+                datum = (still_id, label_id, int(x), int(y))
+                if not db.execute(
+                        "SELECT label_id FROM labels WHERE "
+                        "still_id=? AND label_id=? AND x=? AND y=?", datum).fetchone():
+                    logging.debug(f"\tinserting point{datum[1:]} into database")
+                    db.execute(
+                        "INSERT INTO labels (still_id, label_id, x, y) "
+                        "VALUES (?, ?, ?, ?);", datum)
+                    db.commit()
+            elif s['shape_type'] == 'rectangle':
+                pts = s['points']
+                assert len(pts) == 2
+                (x0, y0), (x1, y1) = pts[0], pts[1]
+                top, bottom = (y0, y1) if y0 < y1 else (y1, y0)
+                left, right = (x0, x1) if x0 < x1 else (x1, x0)
+                # check for new bbox_label
+                if s['label'] not in rbbox_labels:
+                    # insert into database
+                    db.execute(
+                        'INSERT INTO bbox_labels (name) VALUES (?);',
+                        (s['label'], ))
+                    db.commit()
+                    # reload labels
+                    bbox_labels, rbbox_labels = load_lookups_from_table('bbox_labels')
+                label_id = rbbox_labels[s['label']]
+                datum = (still_id, bbox_label_id, left, top, right, bottom)
+                if not db.execute(
+                        "SELECT bbox_id FROM bboxes WHERE "
+                        "still_id=? AND label_id=? AND left=? AND top=? "
+                        "AND right=? AND bottom=?", datum).fetchone():
+                    logging.debug(f"\tinserting bbox{datum[1:]} into database")
+                    db.execute(
+                        "INSERT INTO bboxes"
+                        "(still_id, label_id, left, top, right, bottom) "
+                        "VALUES (?, ?, ?, ?, ?, ?);", datum)
+            else:
                 logging.warning(f"\tinvalid shape type {s['shape_type']}")
                 continue
-            pts = s['points']
-            assert len(pts) == 1
-            x, y = pts[0]
-            label_id = rlabels[s['label']]
-            datum = (still_id, label_id, int(x), int(y))
-            if not db.execute(
-                    "SELECT label_id FROM labels WHERE "
-                    "still_id=? AND label_id=? AND x=? AND y=?", datum).fetchone():
-                logging.debug(f"\tinserting point{datum[1:]} into database")
-                db.execute(
-                    "INSERT INTO labels (still_id, label_id, x, y) "
-                    "VALUES (?, ?, ?, ?);", datum)
-                db.commit()
 
 # write annotations to disk
 db.commit()
