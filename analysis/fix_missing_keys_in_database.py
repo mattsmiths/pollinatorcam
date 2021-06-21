2b3a1b7463106169f9d869a8f5d5421ad7c0c057
Brett <brettgraham@gmail.com>
2021-06-21 11:27:50 -0400

    working on script to fix database schema and missing primary key issues

--- analysis/fix_missing_keys_in_database.py
+++ analysis/fix_missing_keys_in_database.py
@@ -18,12 +18,12 @@ primary_keys_by_table = {
 
 schema_by_table = {
     # skip cameras, configs, detections, videos, stills, crude_results
-    'tags': "CREATE TABLE tags (annotation_id INTEGER PRIMARY KEY, still_id INTEGER, tag_id INTEGER);",
-    'tag_names': "CREATE TABLE tag_names (tag_id INTEGER PRIMARY KEY, name TEXT);",
-    'labels': "CREATE TABLE labels (annotation_id INTEGER PRIMARY KEY, still_id INTEGER, label_id INTEGER, x INTEGER, y INTEGER);",
-    'label_names': "CREATE TABLE label_names (label_id INTEGER PRIMARY KEY, name TEXT);",
-    'bboxes': "CREATE TABLE bboxes (bbox_id INTEGER PRIMARY KEY, still_id INTEGER, label_id INTEGER, left REAL, top REAL, right REAL, bottom REAL);",
-    'bbox_labels': "CREATE TABLE bbox_labels (bbox_label_id INTEGER PRIMARY KEY, name TEXT);",
+    'tags': "CREATE TABLE {} (annotation_id INTEGER PRIMARY KEY, still_id INTEGER, tag_id INTEGER);",
+    'tag_names': "CREATE TABLE {} (tag_id INTEGER PRIMARY KEY, name TEXT);",
+    'labels': "CREATE TABLE {} (annotation_id INTEGER PRIMARY KEY, still_id INTEGER, label_id INTEGER, x INTEGER, y INTEGER);",
+    'label_names': "CREATE TABLE {} (label_id INTEGER PRIMARY KEY, name TEXT);",
+    'bboxes': "CREATE TABLE {} (bbox_id INTEGER PRIMARY KEY, still_id INTEGER, label_id INTEGER, left REAL, top REAL, right REAL, bottom REAL);",
+    'bbox_labels': "CREATE TABLE {} (bbox_label_id INTEGER PRIMARY KEY, name TEXT);",
 }
 
 db_fn = 'pcam.sqlite'
@@ -51,20 +51,42 @@ for table_row in db.execute("SELECT * FROM sqlite_master WHERE type='table';").f
     if name not in primary_keys_by_table:
         raise Exception(f"Found unknown table[{name}] with missing primary key")
 
-    # check for values where what should be the primary key is None
+    # check for 'bad' values: where primary key is None or not unique
     primary_key = primary_keys_by_table[name]
+
+    # check for values where what should be the primary key is None
     bad_values = db.execute(f'SELECT * FROM {name} WHERE {primary_key} IS ?', (None, )).fetchall()
     if len(bad_values):
-        print("\tfound {len(bad_values)} rows where primary key[{primary_key}] is None")
-        print("\t\tExample: {bad_values[0]}")
-        response = input("\tWould you like to delete these rows? [y]es/[n]o")
+        print(f"\tfound {len(bad_values)} rows where primary key[{primary_key}] is None")
+        print(f"\t\tExample: {bad_values[0]}")
+        response = input(f"\tWould you like to delete these rows? [y]es/[n]o >> ")
         if len(response) and response.strip().lower()[0] == 'y':
-            print("\t\tDeleting {len(bad_values)} rows")
-            db.execute('DELETE FROM {name} WHERE {primary_key} IS ?', (None, ))
+            print(f"\t\tDeleting {len(bad_values)} rows")
+            db.execute(f'DELETE FROM {name} WHERE {primary_key} IS ?', (None, ))
             db.commit()
             bad_values = db.execute(f'SELECT * FROM {name} WHERE {primary_key} IS ?', (None, )).fetchall()
             if len(bad_values):
                 raise Exception("Failed to delete bad values")
+
+    # check for non-unique values
+    non_unique_ids = db.execute(f'SELECT {primary_key}, count(*) FROM {name} GROUP BY {primary_key} HAVING count(*) > 1').fetchall()
+    if len(non_unique_ids):
+        print(f"\tFound {len(non_unique_ids)} non-unique primary keys")
+        response = input(f"\tWould you like to delete these rows? [y]es/[n]o >> ")
+        if len(response) and response.strip().lower()[0] == 'y':
+            for non_unique_id in non_unique_ids:
+                bad_id, count = non_unique_id
+                print(f"\tfound {count} rows with the same id {bad_id}")
+                example = db.execute(f'SELECT * FROM {name} WHERE {primary_key}=?', (bad_id, )).fetchone()
+                print(f"\t\tExample: {example}")
+                print(f"\t\tDeleting {count} rows")
+                db.execute(f'DELETE FROM {name} WHERE {primary_key}=?', (bad_id, ))
+                db.commit()
+                vs = db.execute(f'SELECT * FROM {name} WHERE {primary_key}=?', (bad_id, )).fetchall()
+                if len(vs):
+                    raise Exception(f"Failed to delete rows with id {bad_id}")
+
+    # check for values where the pi
 
     # get number of values from old table
     n_values = db.execute(f'SELECT COUNT(*) FROM {name};').fetchone()[0]
@@ -77,12 +99,18 @@ for table_row in db.execute("SELECT * FROM sqlite_master WHERE type='table';").f
     if name not in schema_by_table:
         raise Exception(f"Missing schema for table {name}")
 
-    schema = schema_by_table[name]
+    schema = schema_by_table[name].format(new_name)
     db.execute(schema)
 
     # copy over values
-    db.execute(f"INSERT INTO {new_name} SELECT * FROM {name}")
-    db.commit()
+    if name == 'bboxes':
+        bboxes = db.execute("SELECT bbox_id, still_id, label_id, left, top, right, bottom FROM bboxes").fetchall()
+        for bbox in bboxes:
+            db.execute(f"INSERT INTO {new_name} (bbox_id, still_id, label_id, left, top, right, bottom) VALUES (?, ?, ?, ?, ?, ?, ?)", bbox)
+        db.commit()
+    else:
+        db.execute(f"INSERT INTO {new_name} SELECT * FROM {name}")
+        db.commit()
     new_n_values = db.execute(f'SELECT COUNT(*) FROM {new_name};').fetchone()[0]
     if new_n_values != n_values:
         raise Exception(f"Failed to copy over values: {new_n_values} != {n_values}")
