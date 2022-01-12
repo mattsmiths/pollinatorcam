32a9f3bcf58eeef916cb9ba32df499e807a656a1
Brett <brettgraham@gmail.com>
2022-01-12 12:58:37 -0500

    swithc v4l2ctl output parsing to return list

--- pollinatorcam/v4l2ctl.py
+++ pollinatorcam/v4l2ctl.py
@@ -7,7 +7,7 @@ def get_device_info():
     # output contains names (and busses)
     # followed by devices (on new lines) then a blank line
     device_name = None
-    device_info = {}
+    device_info = []
     for l in o.splitlines():
         # skip blank lines
         if len(l.strip()) == 0:
@@ -15,19 +15,20 @@ def get_device_info():
         
         # if this is a device name, add to devices
         if '/dev/' in l:
-            if device_name not in device_info:
+            if len(device_info) == 0:
                 raise Exception(
                     "Failed to parse v4l2-ctl output, missing device name")
-            device_info[device_name]['devices'].append(l.strip())
+            device_info[-1]['devices'].append(l.strip())
         else:  # not a device line and not blank so this is the next name and bus
             print(l)
             # parse info line into name and bus
             device_name = re.search('(.*)\(', l).groups()[0].strip()
             # get bus in form: usb-0000:01:00.0-1.4.3.4'
             bus = re.search('\((.*)\)', l).groups()[0]
-            device_info[device_name] = {
+            device_info.append({
+                'name': device_name,
                 'info': l,
                 'devices': [],
                 'bus': bus,
-            }
+            })
     return device_info
