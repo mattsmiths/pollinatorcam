053ec7445269416ccf4020f2ef6956dbaec748b6
Brett Graham <brettgraham@gmail.com>
2022-02-05 14:53:33 -0500

    update services for usb cameras

--- services/run_pcam.sh
+++ services/run_pcam.sh
@@ -3,7 +3,9 @@
 source $HOME/.bashrc
 source $HOME/.virtualenvs/pollinatorcam/bin/activate
 
-cd $HOME/r/cbs-ntcore/pollinatorcam
+cd $HOME/r/braingram/pollinatorcam
 
 # exec here to use same PID to allow systemd watchdog
-exec python3 -m pollinatorcam -l $1 -rdD
+#exec python3 -m pollinatorcam -l $1 -rdD
+# TODO flag for usb vs IP camera
+exec python3 -m pollinatorcam -cDrt -l $1 -v
--- services/run_tfliteserve.sh
+++ services/run_tfliteserve.sh
@@ -5,5 +5,5 @@ source $HOME/.virtualenvs/pollinatorcam/bin/activate
 cd $HOME/r/braingram/tfliteserve
 
 # TODO add environment variable to disable EDGE
-python3 -m tfliteserve -m 200123_2035/model.tflite -l 200123_2035/labels.txt -j -1
-#python -m tfliteserve -m 200123_2035/model_edgetpu.tflite -l 200123_2035/labels.txt -e -j -1
+#python3 -m tfliteserve -m 200123_2035/model.tflite -l 200123_2035/labels.txt -j -1
+python -m tfliteserve -m 200123_2035/model_edgetpu.tflite -l 200123_2035/labels.txt -e -j -1
