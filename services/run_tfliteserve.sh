db6a3ce3b3ac956ea4d9af053aa4644d7149fd79
Brett Graham <brettgraham@gmail.com>
2022-02-17 11:42:53 -0500

    switch tfliteserve to non-edge model

--- services/run_tfliteserve.sh
+++ services/run_tfliteserve.sh
@@ -8,4 +8,5 @@ cd $HOME/r/braingram/tfliteserve
 #python3 -m tfliteserve -m 200123_2035/model.tflite -l 200123_2035/labels.txt -j -1
 #python -m tfliteserve -m 200123_2035/model_edgetpu.tflite -l 200123_2035/labels.txt -e -j -1
 #python -m tfliteserve -m 220214/efficientdet-lite_320x320_iNat_II_edgetpu.tflite -l 220214/efficientdet-lite_320x320_iNat_II-labels.txt -e -j -1 -T detector
-python -m tfliteserve -m 220214_one_class/efficientdet-lite_320x320_iNat_insectDetect_edgetpu.tflite -l 220214_one_class/efficientdet-lite_320x320_iNat_insectDetect-labels.txt -e -j -1 -T detector
+#python -m tfliteserve -m 220214_one_class/efficientdet-lite_320x320_iNat_insectDetect_edgetpu.tflite -l 220214_one_class/efficientdet-lite_320x320_iNat_insectDetect-labels.txt -e -j -1 -T detector
+python -m tfliteserve -m 220214_one_class/efficientdet-lite_320x320_iNat_insectDetect.tflite -l 220214_one_class/efficientdet-lite_320x320_iNat_insectDetect-labels.txt -j -1 -T detector
