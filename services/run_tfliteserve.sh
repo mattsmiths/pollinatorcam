4615fa6d01e5952dbd6a782082bf740a56578423
mattsmiths <masmith56@wisc.edu>
2023-05-24 17:46:56 -0500

    Read config, add conditionals

--- services/run_tfliteserve.sh
+++ services/run_tfliteserve.sh
@@ -10,4 +10,22 @@ cd $HOME/r/braingram/tfliteserve
 #python -m tfliteserve -m 220214/efficientdet-lite_320x320_iNat_II_edgetpu.tflite -l 220214/efficientdet-lite_320x320_iNat_II-labels.txt -e -j -1 -T detector
 #python -m tfliteserve -m 220214_one_class/efficientdet-lite_320x320_iNat_insectDetect_edgetpu.tflite -l 220214_one_class/efficientdet-lite_320x320_iNat_insectDetect-labels.txt -e -j -1 -T detector
 #python -m tfliteserve -m 220214_one_class/efficientdet-lite_320x320_iNat_insectDetect.tflite -l 220214_one_class/efficientdet-lite_320x320_iNat_insectDetect-labels.txt -j -1 -T detector
-python -m tfliteserve -m tflite_20220630_1/model.tflite -l tflite_20220630_1/labels.txt -j -1 -T detector
+#python -m tfliteserve -m tflite_20220630_1/model.tflite -l tflite_20220630_1/labels.txt -j -1 -T detector
+
+# Read in model options
+CLASSES=`cat /home/pi/Desktop/configs | jq '.classes'`
+CORAL=`cat /home/pi/Desktop/configs | jq '.coral'`
+
+if [[ $CLASSES == *"multi"* ]]; then
+	if [ $CORAL == 0 ]; then
+		python -m tfliteserve -m tflite_2023/ssd_multi.tflite -l tflite_2023/multi.txt -j -1 -T detector
+	else
+		python -m tfliteserve -m tflite_2023/ssd_multi_edge.tflite -l tflite_2023/multi.txt -e -j -1 -T detector
+	fi
+else
+	if [ $CORAL == 0 ]; then
+		python -m tfliteserve -m tflite_2023/ssd_single.tflite -l tflite_2023/single.txt -j -1 -T detector
+	else
+		python -m tfliteserve -m tflite_2023/ssd_single_edge.tflite -l tflite_2023/single.txt -e -j -1 -T detector
+	fi
+fi
